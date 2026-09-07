80fba99f6f509b9656e825eb70e23c54d6e94f4d
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-07 23:05:54 +0700

    registerview

--- GanChat/Views/RegisterView.swift
+++ GanChat/Views/RegisterView.swift
@@ -14,35 +14,48 @@ struct RegisterView: View {
     @State private var confirmPassword: String = ""
     
     var body: some View {
-        VStack {
-            Text("Welcome")
-                .font(.title)
-                .bold()
-            
-            Text("Register with your email and password.")
-            
-            Spacer()
-            
-            TextField("Username", text: $username)
-                .textFieldStyle(.roundedBorder)
-                .autocorrectionDisabled(true)
-            
-            TextField("Email", text: $email)
-                .textFieldStyle(.roundedBorder)
-                .keyboardType(.emailAddress)
-                .autocapitalization(.none)
-            
-            SecureField("Password", text: $password)
-                .textFieldStyle(.roundedBorder)
-            
-            SecureField("Confirm Password", text: $confirmPassword)
-                .textFieldStyle(.roundedBorder)
-            Spacer()
-            
-            Button("Register") {
-                register()
+        VStack(spacing: 16) {
+            VStack (spacing: 4) {
+                Text("Welcome")
+                    .font(.title)
+                    .bold()
+                
+                Text("Register with your email and password.")
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
             }
-            .buttonStyle(.bordered)
+            .padding(.top, 40)
+            
+            Spacer()
+            
+            VStack(spacing: 12) {
+                TextField("Username", text: $username)
+                    .textFieldStyle(.roundedBorder)
+                    .autocorrectionDisabled(true)
+                
+                TextField("Email", text: $email)
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(.none)
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(.roundedBorder)
+                
+                SecureField("Confirm Password", text: $confirmPassword)
+                    .textFieldStyle(.roundedBorder)
+            }
+            
+            Spacer()
+            
+            Button {
+                register()
+            } label: {
+                Text("Register")
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 4)
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.primary)
         }
         .padding()
     }
