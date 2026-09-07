5c2c60689f1019b9478f9047a6875b0c453cd753
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-07 23:39:36 +0700

    improve ux

--- GanChat/Views/RegisterView.swift
+++ GanChat/Views/RegisterView.swift
@@ -14,50 +14,58 @@ struct RegisterView: View {
     @State private var confirmPassword: String = ""
     
     var body: some View {
-        VStack(spacing: 16) {
-            VStack (spacing: 4) {
-                Text("Welcome")
-                    .font(.title)
-                    .bold()
+        NavigationStack {
+            VStack(spacing: 16) {
+                VStack (spacing: 4) {
+                    Text("Welcome")
+                        .font(.title)
+                        .bold()
+                    
+                    Text("Register with your email and password.")
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                }
+                .padding(.top, 40)
                 
-                Text("Register with your email and password.")
-                    .font(.subheadline)
-                    .foregroundStyle(.secondary)
+                Spacer()
+                
+                VStack(spacing: 12) {
+                    TextField("Username", text: $username)
+                        .textFieldStyle(.roundedBorder)
+                        .autocorrectionDisabled(true)
+                    
+                    TextField("Email", text: $email)
+                        .textFieldStyle(.roundedBorder)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                    
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    SecureField("Confirm Password", text: $confirmPassword)
+                        .textFieldStyle(.roundedBorder)
+                }
+                
+                Spacer()
+                
+                Button {
+                    register()
+                } label: {
+                    Text("Register")
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 4)
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.primary)
+                
+                NavigationLink {
+                    LoginView()
+                } label: {
+                    Text("Already have account")
+                }
             }
-            .padding(.top, 40)
-            
-            Spacer()
-            
-            VStack(spacing: 12) {
-                TextField("Username", text: $username)
-                    .textFieldStyle(.roundedBorder)
-                    .autocorrectionDisabled(true)
-                
-                TextField("Email", text: $email)
-                    .textFieldStyle(.roundedBorder)
-                    .keyboardType(.emailAddress)
-                    .autocapitalization(.none)
-                
-                SecureField("Password", text: $password)
-                    .textFieldStyle(.roundedBorder)
-                
-                SecureField("Confirm Password", text: $confirmPassword)
-                    .textFieldStyle(.roundedBorder)
-            }
-            
-            Spacer()
-            
-            Button {
-                register()
-            } label: {
-                Text("Register")
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 4)
-            }
-            .buttonStyle(.borderedProminent)
-            .tint(.primary)
+            .padding()
         }
-        .padding()
     }
     
     func register() {
@@ -66,5 +74,7 @@ struct RegisterView: View {
 }
 
 #Preview {
-    RegisterView()
+    NavigationStack {
+        RegisterView()
+    }
 }
