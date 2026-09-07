3a83f34a6a022858e7d36c565aa4fa1f1c2db580
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-07 20:50:01 +0700

    register view

--- GanChat/Views/RegisterView.swift
+++ GanChat/Views/RegisterView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State private var username: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Welcome")
+                .font(.title)
+                .bold()
+            
+            Text("Register with your email and password.")
+            
+            TextField("Username", text: $username)
+                .textFieldStyle(.roundedBorder)
+            
+            TextField("Email", text: $email)
+                .textFieldStyle(.roundedBorder)
+            
+            TextField("Password", text: $password)
+                .textFieldStyle(.roundedBorder)
+            
+            TextField("Confirm Password", text: $confirmPassword)
+                .textFieldStyle(.roundedBorder)
+            
+            Button("Register") {
+                register()
+            }
+            .padding(.top)
+            .buttonStyle(.bordered)
+        }
+        .padding()
+    }
+    
+    func register() {
+        print("username: \(username) | email: \(email) | password: \(password) | confirmPassword: \(confirmPassword)")
     }
 }
 
