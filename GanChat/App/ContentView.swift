11a0a4b047ce01d15f86c7245d6c98386375480f
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-07 20:21:52 +0700

    architecture

---
+++ GanChat/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GanChat
+//
+//  Created by chuonpiseth on 6/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ New Group/GanChat/GanChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GanChatApp.swift
+//  GanChat
+//
+//  Created by chuonpiseth on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct GanChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
