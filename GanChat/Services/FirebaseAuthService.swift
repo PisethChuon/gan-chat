79fc48c1f78710623423d00f8cc2c4ca1df005cc
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-12 15:45:30 +0700

    feat: firebaseAuthenServices

--- GanChat/Services/FirebaseAuthService.swift
+++ GanChat/Services/FirebaseAuthService.swift
@@ -4,3 +4,35 @@
 //
 //  Created by chuonpiseth on 8/9/26.
 //
+import Foundation
+import FirebaseAuth
+
+final class FirebaseAuthService {
+    
+    //  Singleton
+    static let shared = FirebaseAuthService()
+    //  Prevent everyone else use from creating another instance
+    private init() {}
+    
+    @discardableResult
+    func register(
+        email: String,
+        password: String,
+    ) async throws -> AuthDataResult {
+        try await Auth.auth().createUser(withEmail: email, password: password)
+    }
+    
+    @discardableResult
+    func login(
+        email: String,
+        password: String
+    ) async throws -> AuthDataResult {
+        try await Auth.auth().signIn(withEmail: email, link: password)
+    }
+    
+    func logout() throws {
+        try Auth.auth().signOut()
+    }
+    
+    
+}
