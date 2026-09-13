7626555bdd37750e7b3b1424ef155bbc4aea70a6
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-13 17:17:21 +0700

    feat: func-login

--- GanChat/ViewModels/LoginViewModel.swift
+++ GanChat/ViewModels/LoginViewModel.swift
@@ -6,9 +6,52 @@
 //
 
 import Foundation
+import Observation
+import FirebaseAuth
 
+@MainActor
 @Observable
-class LoginViewModel {
+final class LoginViewModel {
+    private let authService: FirebaseAuthService
+    private var errorMessage = ""
+    private var isLoading = false
+    
+    init(authService: FirebaseAuthService = .shared) {
+        self.authService = authService
+    }
+    
     var email: String = ""
     var password: String = ""
+    
+    func login() async {
+        let trimmedEmail = email.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard !trimmedEmail.isEmpty else {
+            errorMessage = "Please enter you email"
+            return
+        }
+        
+        let trimmedPassword = password.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard !trimmedPassword.isEmpty else {
+            errorMessage = "Please enter you password"
+            return
+        }
+        
+        isLoading = true
+        errorMessage = ""
+        
+        defer {
+            isLoading = false
+        }
+        
+        do {
+            try await authService.login(
+                email: trimmedEmail,
+                password: trimmedPassword
+            )
+        } catch {
+            return
+        }
+    }
 }
