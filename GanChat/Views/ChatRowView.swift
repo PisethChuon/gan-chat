79765b15e309627e6d105c760006850149b17bb9
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-14 23:03:50 +0700

    feat: initial-chat-row

---
+++ GanChat/Views/ChatRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatRowView.swift
+//  GanChat
+//
+//  Created by chuonpiseth on 14/9/26.
+//
+
+import SwiftUI
+
+struct ChatRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatRowView()
+}
