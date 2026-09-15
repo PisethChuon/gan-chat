b173e508cf3d5cbeeddbfd9fb7a7bbf66450719f
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-15 22:02:30 +0700

    feat: initial-chat-message-model

--- GanChat/Models/ChatMessage.swift
+++ GanChat/Models/ChatMessage.swift
@@ -5,3 +5,23 @@
 //  Created by chuonpiseth on 15/9/26.
 //
 
+import Foundation
+
+struct ChatMessage {
+    let id: UUID
+    let text: String
+    let sentAt: Date
+    let isFromCurrentUser: Bool
+    
+    init (
+        id: UUID = UUID(),
+        text: String,
+        sentAt: Date = Date(),
+        isFromCurrentUser: Bool
+    ) {
+        self.id = id
+        self.text = text
+        self.sentAt = sentAt
+        self.isFromCurrentUser = isFromCurrentUser
+    }
+}
