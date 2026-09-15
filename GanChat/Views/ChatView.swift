f76acdda7397086a01c35205779413ca36a1ef65
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-16 00:07:23 +0700

    feat: initial-chat-view

---
+++ GanChat/Views/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  GanChat
+//
+//  Created by chuonpiseth on 16/9/26.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
