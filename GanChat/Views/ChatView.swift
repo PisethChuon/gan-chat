80b93295027c81db4edd80c479dbc461c692ff29
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-16 01:00:39 +0700

    feat: initial-chatview

--- GanChat/Views/ChatView.swift
+++ GanChat/Views/ChatView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct ChatView: View {
+    @State private var viewModel: ChatViewModel
+    
+    init(chat: ChatRowViewModel) {
+        _viewModel = State(
+            initialValue: ChatViewModel(chat: chat)
+        )
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
 #Preview {
-    ChatView()
+    NavigationStack {
+        ChatView(chat: ChatRowViewModel.mockChats[0])
+    }
 }
