1c8358c99ea98094608d299101edd0e7de67bc1a
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-18 23:39:46 +0700

    feat: user-search-view

---
+++ GanChat/Views/UserSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  UserSearchView.swift
+//  GanChat
+//
+//  Created by chuonpiseth on 18/9/26.
+//
+
+import SwiftUI
+
+struct UserSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserSearchView()
+}
