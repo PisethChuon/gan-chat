79490637cbe723f181a40ccb53f69f40a381c93f
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-23 13:20:34 +0700

    chore: big idea concept

--- GanChat/Services/FirestoreConversationRepository.swift
+++ GanChat/Services/FirestoreConversationRepository.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+// The Big idea
+// Bob want to chat with Alice
+//  FirestoreConversationRepository
+//      Validate Bob + Alice
+//      Create a deterministic conversation ID
+//      Look for that conversation in Firestore
+//      Found ? return it
+//      Not found –> create it –> return it
+
+
+// func getOrCreateconversation
+// func makeConversation
+// func makeConversationID
